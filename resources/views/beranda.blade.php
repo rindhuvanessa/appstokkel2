a373ee80b77209acf2a58abebdc73356aa1705a8
unknown <tugassafirabrkhpct@gmail.com>
2024-02-26 12:15:03 +0700

    commit

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -7,17 +7,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="card">
-         <div class="card-header">
-          Beranda
-         </div>
-         <div class="card-body">
-           <h3 class="card-title">Selamat Datang</h3>
-           <h5 class="card-title">Aplikasi Stock Barang PT.SAN PALEMBANG.</h5>
-        <p class="card-text">Jl.Kertapati</p>
-        <p class="card-text">087823456132</p>
-        <p href="{{ route('login') }}" class="btn btn-primary">Login</a>
-        </div>
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
+        <div class="col-md-6 p-lg-5 mx-auto my-5">
+        <h1 class="display-3 fw-bold">Selamat Datang Di</h1>
+        <h3 class="fw-bold text-muted mb-3">PT. SAN INDONESIA</h3>
+        <h3 class="fw-normal text-muted mb-3">Terima Kasih telah mengunjungi PT. SAN INDONESIA</h3>
+        <div class="d-flex gap-3 justify-content-center lead fw-normal">
+        <a class="icon-link" href="#">
+        Masuk
+        <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a>
+      </div>
+    </div>
+      <div class="product-device shadow-sm d-none d-md-block"></div>        
+      <div class="product-device product-device-2 shadow-sm d-none d-md-block">
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
