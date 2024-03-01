1c661a359177d2c7bc5a1a260d361346550042dc
unknown <tugassafirabrkhpct@gmail.com>
2024-03-01 15:37:45 +0700

    commit

--- resources/views/suplayer/insert.blade.php
+++ resources/views/suplayer/insert.blade.php
@@ -3,15 +3,15 @@
 <form>
   <div class="mb-3">
     <label for="namasupplier" class="form-label">Namasupplier</label>
-    <input type="namasupplier" class="form-control" id="namasupplier" aria-describedby="Safira">
+    <input type="namasupplier" class="form-control" id="namasupplier" aria-describedby="masukan nama suplayer">
   </div>
   <div class="mb-3">
     <label for="nomorwhatsapp" class="form-label">Nomorwhatsapp</label>
-    <input type="nomorwhatsapp" class="form-control" id="nomorwhatsapp" aria-describedby="081234567892">
+    <input type="nomorwhatsapp" class="form-control" id="nomorwhatsapp" aria-describedby="masukan nomor whatsapp suplayer">
   </div>
   <div class="mb-3">
     <label for="alamatperusahaan" class="form-label">Alamatperusahaan</label>
-    <textarea type="alamatperusahaan" id="alamatperusahaan" cols="30" rows="10" aria-describedby="Jl. Jakabaring"></textarea>
+    <textarea type="alamatperusahaan" id="alamatperusahaan" cols="30" rows="10" aria-describedby="tuliskan alamat"></textarea>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
