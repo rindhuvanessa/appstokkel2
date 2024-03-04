8395a52e5ef24b19641e9382fdf6de6ae4d8d096
unknown <tugassafirabrkhpct@gmail.com>
2024-03-04 15:58:11 +0700

    commit

---
+++ resources/views/suplayer/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layout')
+@section('content')
+<form action="/savesuplayer" method="post" enctype="multipart/form-data">
+  @csrf
+  <div class="mb-3"> 
+    <label for="namasuplayer" class="form-label">Nama Suplayer</label>
+    <input type="text" name="namasuplayer" class="form-control" id="namasuplayer" value="{{$suplayer->namasupplier}}">
+  </div>
+  <div class="mb-3" >
+    <label for="nomorwhatsapp" class="form-label">Nomor Whatsapp</label>
+    <input type="text" name="nomorwhatsapp" class="form-control" id="nomorwhatsapp" value="{{$suplayer->nomorwhatsaap}}">
+  </div>
+  <div class="mb-3" >
+    <label for="alamatperusahaan" class="form-label">Alamat Perusahaan</label>
+    <textarea name="alamatperusahaan" class="form-control" id="alamatperusahaan" cols="30" rows="10" aria-describedby="tuliskan alamat">"{{$suplayer->alamatperusahaan}}"</textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+@endsection
