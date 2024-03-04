e4e7a8ff25ab8fa16fb5e498a9305a845700f72c
unknown <alenciaputriptc7@gmail.com>
2024-03-04 17:04:01 +0700

    commit

---
+++ resources/views/bkeluar/bkeluar.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layout')
+@section('content')
+    <h1>bkeluar</h1>
+    <hr>
+    <a class="btn btn-primary" href="tambahbkeluar" role="button">Tambah Barang Keluar</a>
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Tanggal Faktur</th>
+      <th scope="col">Stok</th>
+      <th scope="col">Jumlah</th>
+      <th scope="col">Subtotal</th>
+      <th scope="col">User_Id</th>
+      <th scope="col">Tanggal Buat</th>
+      <th scope="col">Cabang</th>
+    </tr>
+  </thead>
+  <tbody>
