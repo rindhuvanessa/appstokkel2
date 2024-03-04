87701fcce17dd1f59885b4930ff8e26499f9d87f
unknown <tugassafirabrkhpct@gmail.com>
2024-03-04 15:43:04 +0700

    commit

--- resources/views/suplayer/insert.blade.php
+++ resources/views/suplayer/insert.blade.php
@@ -2,17 +2,17 @@
 @section('content')
 <form action="/savesuplayer" method="post" enctype="multipart/form-data">
   @csrf
-  <div> 
+  <div class="mb-3"> 
     <label for="namasuplayer" class="form-label">Nama Suplayer</label>
     <input type="text" name="namasuplayer" class="form-control" id="namasuplayer" aria-describedby="masukan nama suplayer">
   </div>
-  <div>
+  <div class="mb-3" >
     <label for="nomorwhatsapp" class="form-label">Nomor Whatsapp</label>
     <input type="text" name="nomorwhatsapp" class="form-control" id="nomorwhatsapp" aria-describedby="masukan nomor whatsapp suplayer">
   </div>
-  <div>
+  <div class="mb-3" >
     <label for="alamatperusahaan" class="form-label">Alamat Perusahaan</label>
-    <textarea class="form-control" id="alamatperusahaan" cols="30" rows="10" aria-describedby="tuliskan alamat"></textarea>
+    <textarea name="alamatperusahaan" class="form-control" id="alamatperusahaan" cols="30" rows="10" aria-describedby="tuliskan alamat"></textarea>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
